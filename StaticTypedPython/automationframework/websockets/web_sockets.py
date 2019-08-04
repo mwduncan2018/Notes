fb17c37df6c64dd8d5488037ebd4a9e3152d95ac
Mike Duncan <mduncan2600@gmail.com>
2019-08-04 14:00:57 -0400

    updates

--- StaticTypedPython/automationframework/websockets/web_sockets.py
+++ StaticTypedPython/automationframework/websockets/web_sockets.py
@@ -1,30 +1,46 @@
 #!/usr/bin/env python3
 """This is a demo for static typing in Python"""
 
+from typing import *
+
 class WebSockets:
 
     def __init__(self):
         self._messages = [] # type: List[str]
+        self._active = False # type: bool
 
     def activate(self) -> str:
+        self._active = True
         return "Web Sockets Activated"
 
     def deactivate(self) -> str:
+        self._active = False
         return "Web Sockets Deactivated"
 
     def receive_message(self, message: str) -> str:
-        self._messages.append(message)
-        return message
+        if self._active == True:
+            self._messages.append(message)
+            return message
+        else:
+            raise Exception()
 
     def write_messages(self) -> int:
-        f = open(r'C:/dev/Python/zen_of_python/TestResults/messages.txt', mode='wt',  encoding='utf-8')
-        for message in self._messages:
-            f.write(message + "\n")
-        return len(self._messages)
+        if self._active == True:
+            f = open(r'C:/All_Python/zen_of_python/StaticTypedPython/TestResults/messages.txt', mode='wt',  encoding='utf-8')
+            for message in self._messages:
+                f.write(message + "\n")
+            f.close()
+            return len(self._messages)
+        else:
+            raise Exception()
 
-    def read_messages(self) -> int:
-        f = open(r'C:/dev/Python/zen_of_python/TestResults/messages.txt', mode='wt',  encoding='utf-8')
-        self._messages = []
-        for line in f.readlines():
-            self._messages.append(line)
-        return len(self._messages)
+    def read_messages(self) -> List[str]:
+        if self._active == True:
+            f = open(r'C:/All_Python/zen_of_python/StaticTypedPython/TestResults/messages.txt', mode='rt',  encoding='utf-8')
+            self._messages = []
+            for line in f.readlines():
+                self._messages.append(line)
+            f.close()
+            return self._messages
+        else:
+            raise Exception()
--- StaticTypedPython/test_cases.py
+++ StaticTypedPython/test_cases.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
 """This is a demo for static typing in Python"""
 
-from .automationframework.websockets.web_sockets import WebSockets
+from automationframework.websockets.web_sockets import WebSockets
 
 class TestCases:
 
     def __init__(self):
-        self._web_sockets = WebSockets()
+        self._web_sockets = None # type: WebSockets
 
-    def verify_a_web_socket_can_be_activated(self) -> bool:
+    def verify_a_web_socket_can_be_activated(self):
         self._web_sockets = WebSockets()
         result = self._web_sockets.activate()
         assert result == 'Web Sockets Activated'
+        self._web_sockets.deactivate()
 
-    def verify_a_web_socket_can_be_shut_down(self) -> bool:
+    def verify_a_web_socket_can_be_shut_down(self):
         self._web_sockets = WebSockets()
         self._web_sockets.activate()
-        result = self._web_sockets.deactivate
+        result = self._web_sockets.deactivate()
         assert result == 'Web Sockets Deactivated'
 
-    def verify_a_web_socket_can_receive_a_message(self) -> bool:
+    def verify_a_web_socket_can_receive_a_message(self):
         self._web_sockets = WebSockets()
         self._web_sockets.activate()
         test_str = 'Why I have believed as many as seven things before breakfast'
@@ -27,5 +28,11 @@ class TestCases:
         assert result == test_str
         assert self._web_sockets._messages[0] == test_str
 
+    def verify_a_web_socket_can_write_a_message(self):
+        self._web_sockets = WebSockets()
+        self._web_sockets.activate()
+        self._web_sockets.receive_message('Four score and seven years ago')
+        self._web_sockets.write_messages()
+        result = self._web_sockets.read_messages()
 
 
