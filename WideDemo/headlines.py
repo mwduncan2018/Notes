f3df0e856d063ecf8455b97c1528e4f567d00d44
Mike Duncan <mduncan2600@gmail.com>
2019-08-04 12:17:42 -0400

    updates

--- WideDemo/headlines.py
+++ WideDemo/headlines.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
 """This is a demo for static typing in Python"""
 
+class City:
+    def __init__(self, name):
+        self._name = name
+
+    def get_crime_rate(self) -> str:
+        if self._name == 'Baltimore':
+            return 'Bad'
+        else:
+            return 'Ok'
+
+    def get_mayor_name(self) -> str:
+        if self._name == 'Baltimore':
+            return "Martin O'Malley"
+        else:
+            return 'John Locke'
 
 
-
+def headline(text: str, city: City, align: bool = True) -> str:
+    result = [text, city._name, ('Crime is ' + city.get_crime_rate())]
+    if align:
+        result.insert(0, 'aligned')
+    return ' -- '.join(result)
 
 
 if __name__ == '__main__':
-    pass
+    baltimore = City('Baltimore')
+    print(headline('testing', baltimore, True))
